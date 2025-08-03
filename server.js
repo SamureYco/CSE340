19bf3a884976c228027dd2f240a27b661846b170
SamuelReyes6671 <samuel.reyes.condori@email.com>
2025-08-03 02:12:22 -0500

    images ready

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use(function(req, res, next){
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
+app.use(express.static("public"))
 
 /* ***********************
  * View Engine and Templates
